98f18c8ad864000caa80d834e8870013496df679
shahzaibriaz69 <shahzaibriaz0023@gmail.com>
2025-12-11 17:46:03 +0500

    Add login form

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ let cartbtn = document.querySelector('#cart-btn');
 cartbtn.onclick = () =>{
   shoppingCart.classList.toggle('active');
 }
+
+let loginForm = document.querySelector('.login-form');
+let loginbtn = document.querySelector('#login-btn');  
+loginbtn.onclick = () =>{
+  loginForm.classList.toggle('active');
+}
